b18ac66aeaf345daf2180d51a2431dbe9136e228
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 15:25:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,3 +43,13 @@ def reduce_to_total(source_array, starting_point = 0)
   total
 end
 
+def reduce_to_all_true(source_array)
+  
+  for element in source_array
+    if !element 
+      return false
+    end
+  end
+  true
+end
+
