4ac70a1b684e7e8edcb948791df45b6af58ff06e
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 15:22:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,7 +33,7 @@ def map_to_square(source_array)
   dest_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0 )
   total = 0 
   
   for index in starting_point...source_array.size
